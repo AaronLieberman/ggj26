4a1d3d151f59e5be0c93d4a82ddd1db00d921415
Aaron Lieberman <aaronlcode@gmail.com>
2026-01-31 01:29:48 -0800

    Customer images

--- GameJam26/Assets/Scripts/ShopManager.cs
+++ GameJam26/Assets/Scripts/ShopManager.cs
@@ -1,11 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+[System.Serializable]
+public struct CustomerEntry
+{
+    public string name;
+    public Sprite sprite;
+}
 
 public class ShopManager : MonoBehaviour
 {
     public GameObject Customer;
     public Transform CustomerSpawnPoint;
     public float Speed = 1;
+    public CustomerEntry[] Sprites;
     
     GameObject _currentCustomer;
 
@@ -14,7 +24,15 @@ public class ShopManager : MonoBehaviour
         new() {
             customerName = "Amanda Tea",
             customerDialogue = "I'm going to High Tea with my nieces. I need a mask that will make them laugh, and looks very pretty. Bonus points if its animal themed, they love cats!",
-            customerImageName = "amandatea",
+            customerImageName = "AmandaTea",
+            maskScary = { Min = 0, Max = 4 },
+            maskGoofy = { Min = 3, Max = 10, Points = 50 },
+            maskBeauty = { Min = 3, Max = 10, Points = 40 },
+        },
+        new() {
+            customerName = "Francis Lyon",
+            customerDialogue = "I'm going to Spain for the running of the bulls! I want to be a masked hero! Make me mysterious and manly! And make those bulls think twice about chasing me!",
+            customerImageName = "FrancisLyon",
             maskScary = { Min = 0, Max = 4 },
             maskGoofy = { Min = 3, Max = 10, Points = 50 },
             maskBeauty = { Min = 3, Max = 10, Points = 40 },
@@ -25,7 +43,7 @@ public class ShopManager : MonoBehaviour
     {
         if ( _currentCustomer == null )
         {
-            _currentCustomer = SpawnCustomer(_customers[0]);
+            _currentCustomer = SpawnCustomer(_customers[Random.Range(0, _customers.Length)]);
         }
     }
 
@@ -33,6 +51,8 @@ public class ShopManager : MonoBehaviour
     {
         GameObject created = Instantiate(Customer, CustomerSpawnPoint.position, CustomerSpawnPoint.rotation, CustomerSpawnPoint);
         created.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Speed, 0.0f));
+
+        created.GetComponent<Image>().sprite = Sprites.SingleOrDefault(a => a.name == customerData.customerImageName).sprite;
 
         var name = transform.Find("Name");
         var conversation = transform.Find("Conversation");
