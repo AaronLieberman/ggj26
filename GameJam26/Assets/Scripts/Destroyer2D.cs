5bfd4131920f219523ede5c609739d2e2ae9cc4b
Victor Erickson <vericks101@gmail.com>
2026-01-30 22:52:28 -0800

    Updated destroyer 2d log message

--- GameJam26/Assets/Scripts/Destroyer2D.cs
+++ GameJam26/Assets/Scripts/Destroyer2D.cs
@@ -4,7 +4,7 @@ public class Destroyer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.LogFormat("Destroying mask piece: {0}", collision.name);
+        Debug.LogFormat("Destroying collided object: {0}", collision.name);
         Destroy(collision.gameObject);
     }
 }
