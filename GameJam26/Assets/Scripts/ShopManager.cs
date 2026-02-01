3d8a797effe353e67e13683aded8ab085e3245ed
Victor Erickson <vericks101@gmail.com>
2026-02-01 12:05:31 -0800

    Fixed ShopManager references to score calculator.

--- GameJam26/Assets/Scripts/ShopManager.cs
+++ GameJam26/Assets/Scripts/ShopManager.cs
@@ -27,7 +27,7 @@ public class ShopManager : MonoBehaviour
     bool _customerLeaving;
     string _lastDebugCustomerToShow;
     MaskDisplayManager _maskDisplayManager;
-    MaskComputer _maskComputer;
+    ScoreCalculator _scoreCalculator;
 
     TextMeshProUGUI _nameText;
     TextMeshProUGUI _conversationText;
@@ -40,7 +40,7 @@ public class ShopManager : MonoBehaviour
     {
         _customers = CustomerDataLoader.Load();
         _maskDisplayManager = Utilities.GetRootComponentRecursive<MaskDisplayManager>();
-        _maskComputer = Utilities.GetRootComponentRecursive<MaskComputer>();
+        _scoreCalculator = Utilities.GetRootComponentRecursive<ScoreCalculator>();
     }
 
     void Start()
@@ -127,7 +127,7 @@ public class ShopManager : MonoBehaviour
         _currentCustomer.CustomerResult = new CustomerResult(
             _currentCustomer.Data,
             customerMask ? customerMask.gameObject : null,
-            customerMask ? _maskComputer.GetActiveMaskScore() : 0
+            customerMask ? _scoreCalculator.GetActiveMaskScore() : 0
         );
 
         _maskDisplayManager.ClearMaskDisplay();
