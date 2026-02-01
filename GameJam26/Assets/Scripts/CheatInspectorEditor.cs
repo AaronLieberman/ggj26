a8861a6f5aeb8663db95b4137ec64fca61e65bf8
Eric Carter <gitmeongithub@greenstorm.net>
2026-02-01 09:26:47 -0800

    added a cheat panel to set the next customers

---
+++ GameJam26/Assets/Scripts/CheatInspectorEditor.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+[CustomEditor(typeof(CheatInspector))]
+public class CheatInspectorEditor : Editor
+{
+
+    float speedMultiplier = 0;
+
+    private void OnEnable()
+    {
+        ShopManager shopManager = FindObjectOfType<ShopManager>();
+
+        speedMultiplier = shopManager.Speed;
+
+    }
+    public override void OnInspectorGUI()
+    {
+        ShopManager shopManager = FindObjectOfType<ShopManager>();
+
+        //DrawDefaultInspector();
+
+        if (GUILayout.Button("Set Next Customer Amanda Tea"))
+        {
+            shopManager.DebugCustomerToShow = "AmandaTea";
+        }
+
+        GUILayout.Label("Set Next Customer Timer Speed Multiplier. Currently: " + shopManager.SpeedMultiplier);
+        GUILayout.BeginHorizontal();
+        speedMultiplier = EditorGUILayout.FloatField("New Speed Multiplier", speedMultiplier);
+        if (GUILayout.Button("Set Speed Multiplier"))
+        {
+            shopManager.SpeedMultiplier = speedMultiplier;
+        }
+        GUILayout.EndHorizontal();
+    }
+}
