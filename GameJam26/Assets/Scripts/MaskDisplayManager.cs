a0c791765f5eb87866e0e7109380489507aa4cf7
Victor Erickson <vericks101@gmail.com>
2026-01-31 21:03:54 -0800

    Mask deliver button now takes into account various states to tell whether it should be accessible or not.

---
+++ GameJam26/Assets/Scripts/MaskDisplayManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaskDisplayManager : MonoBehaviour
+{
+    private Button maskDeliveryButton;
+    private ShopManager shopManager;
+    private MaskComputer maskComputer;
+
+    private void Awake()
+    {
+        maskDeliveryButton = transform.Find("DeliverButton").GetComponent<Button>();
+        shopManager = Utilities.GetRootComponentRecursive<ShopManager>();
+        maskComputer = Utilities.GetRootComponentRecursive<MaskComputer>();
+    }
+
+    private void Update()
+    {
+        maskDeliveryButton.interactable = shopManager.MaskIsDeliverable && maskComputer.GetActiveMaskAcceptable();
+    }
+}
