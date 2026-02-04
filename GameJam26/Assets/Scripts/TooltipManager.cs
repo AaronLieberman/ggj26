41f3ce0f282df6f497d99a3ce947e8d3efb5d110
Aaron Lieberman <aaronlcode@gmail.com>
2026-02-03 22:18:24 -0800

    Tooltip improvements: always on, positioning

--- GameJam26/Assets/Scripts/TooltipManager.cs
+++ GameJam26/Assets/Scripts/TooltipManager.cs
@@ -12,13 +12,14 @@ class TooltipManager : MonoBehaviour
     public static TooltipManager Instance;
 
     [SerializeField] public PieceTooltip PieceStatsTooltip;
+    [SerializeField] public float TooltipXOffset = 20;
     [SerializeField] public float TooltipYOffset = 140;
 
     private RectTransform _rectTransform;
     private CanvasGroup _canvasGroup;
     private Canvas _canvas;
     private RectTransform _canvasRect;
-    private Camera _canvasCamera;
+    private RectTransform _tooltipRect;
     private Vector2 _lastMousePos = Vector2.zero;
     private MaskPiece _showingOnPiece;
     private void Awake()
@@ -29,10 +30,10 @@ class TooltipManager : MonoBehaviour
 
         _canvas = _rectTransform.GetComponentInParent<Canvas>();
         if (_canvas != null)
-        {
             _canvasRect = _canvas.GetComponent<RectTransform>();
-            _canvasCamera = _canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : _canvas.worldCamera;
-        }
+
+        if (PieceStatsTooltip != null)
+            _tooltipRect = PieceStatsTooltip.GetComponent<RectTransform>();
 
         HideAll();
     }
@@ -41,37 +42,40 @@ class TooltipManager : MonoBehaviour
     {
 #if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
         Vector2 mousePos = Mouse.current != null ? Mouse.current.position.ReadValue() : Vector2.zero;
-        bool mouseDown = Mouse.current != null ? Mouse.current.leftButton.isPressed : false;
 #else
         Vector2 mousePos = Input.mousePosition;
-        bool mouseDown = Input.GetMouseButtonDown(0);
 #endif
         _rectTransform.position = mousePos;
 
         if (_showingOnPiece != null && PieceStatsTooltip != null)
         {
-            PieceStatsTooltip.transform.position = mousePos + new Vector2(0, TooltipYOffset * _canvas.scaleFactor);
+            PositionTooltip(mousePos);
         }
 
-        if (_lastMousePos == mousePos
-            || mouseDown)
-        {
-            if (mouseDown)
-            {
-                HideAll();
-            }
+        if (_lastMousePos == mousePos)
             return;
-        }
 
         _lastMousePos = mousePos;
 
         var conveyorManager = Utilities.GetRootComponentRecursive<ConveyorManager>();
         var piecesOnConveyor = conveyorManager.GetComponentsInChildren<MaskPiece>();
-        
+
         var maskDisplay = GameObject.Find("MaskDisplay");
         var piecesOnMaskDisplay = maskDisplay.GetComponentsInChildren<MaskPiece>();
 
-        foreach (var piece in piecesOnConveyor.Concat(piecesOnMaskDisplay))
+        // Also find pieces being dragged (reparented to canvas root)
+        var allPieces = piecesOnConveyor.Concat(piecesOnMaskDisplay);
+        if (_canvasRect != null)
+        {
+            for (int i = 0; i < _canvasRect.childCount; i++)
+            {
+                var piece = _canvasRect.GetChild(i).GetComponent<MaskPiece>();
+                if (piece != null)
+                    allPieces = allPieces.Append(piece);
+            }
+        }
+
+        foreach (var piece in allPieces)
         {
             var rt = piece.GetComponent<RectTransform>();
             var canvas = piece.GetComponentInParent<Canvas>();
@@ -103,7 +107,7 @@ class TooltipManager : MonoBehaviour
         }
 
         UnityEngine.Debug.Log($"Showing tooltip for piece: {piece.name}");
-        PieceStatsTooltip.transform.position = mousePos + new Vector2(0, TooltipYOffset * _canvas.scaleFactor);
+        PositionTooltip(mousePos);
         PieceStatsTooltip.Data = piece.Data;
     }
 
@@ -122,18 +126,52 @@ class TooltipManager : MonoBehaviour
 
     }
 
-    static Vector2 ClampTooltipLocalPoint(RectTransform tooltipRect, RectTransform canvasRect, Vector2 localPoint, float padding)
+    void PositionTooltip(Vector2 mousePos)
     {
-        Vector2 size = tooltipRect.rect.size;
-        Vector2 pivot = tooltipRect.pivot;
+        if (_tooltipRect == null) return;
 
-        float minX = canvasRect.rect.xMin + padding + size.x * pivot.x;
-        float maxX = canvasRect.rect.xMax - padding - size.x * (1f - pivot.x);
-        float minY = canvasRect.rect.yMin + padding + size.y * pivot.y;
-        float maxY = canvasRect.rect.yMax - padding - size.y * (1f - pivot.y);
+        float scale = _canvas.scaleFactor;
+        float offset = TooltipXOffset * scale;
+        Vector2 size = _tooltipRect.rect.size * scale;
+        Vector2 pivot = _tooltipRect.pivot;
+        Rect safe = GetSafeArea();
 
-        localPoint.x = Mathf.Clamp(localPoint.x, minX, maxX);
-        localPoint.y = Mathf.Clamp(localPoint.y, minY, maxY);
-        return localPoint;
-    }    
+        float yOffset = TooltipYOffset * scale;
+
+        // Place left edge of tooltip at cursor + offset
+        float x = mousePos.x + offset + size.x * pivot.x;
+
+        // If right edge exceeds safe area, flip to left of cursor
+        float rightEdge = x + size.x * (1f - pivot.x);
+        if (rightEdge > safe.xMax)
+            x = mousePos.x - offset - size.x * (1f - pivot.x);
+
+        float y = Mathf.Clamp(mousePos.y + yOffset,
+            safe.yMin + size.y * pivot.y,
+            safe.yMax - size.y * (1f - pivot.y));
+
+        _tooltipRect.position = new Vector2(x, y);
+    }
+
+    Rect GetSafeArea()
+    {
+        float targetAspect = 16f / 9f;
+        float screenAspect = (float)Screen.width / Screen.height;
+
+        if (screenAspect > targetAspect)
+        {
+            float safeWidth = Screen.height * targetAspect;
+            float left = (Screen.width - safeWidth) / 2f;
+            return new Rect(left, 0, safeWidth, Screen.height);
+        }
+
+        if (screenAspect < targetAspect)
+        {
+            float safeHeight = Screen.width / targetAspect;
+            float bottom = (Screen.height - safeHeight) / 2f;
+            return new Rect(0, bottom, Screen.width, safeHeight);
+        }
+
+        return new Rect(0, 0, Screen.width, Screen.height);
+    }
 }
